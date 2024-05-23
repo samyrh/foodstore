3f948306ff1dc11b88ac8e73083a53c1f2a7d7cd
samirh99 <rhalimsami@gmail.com>
2024-05-24 00:50:26 +0100

    all

--- src/main/java/food/ma/foodstore/security/SecurityConfig.java
+++ src/main/java/food/ma/foodstore/security/SecurityConfig.java
@@ -1,4 +1,36 @@
 package food.ma.foodstore.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+
+        return new InMemoryUserDetailsManager(
+                User.withUsername("Sami").password("12345").roles("USER").build()
+        );
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity  httpSecurity) throws Exception {
+
+        httpSecurity.authorizeRequests()
+                .anyRequest().authenticated();
+
+        return httpSecurity.build();
+
+    }
+
+
 }
